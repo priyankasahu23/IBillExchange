6eb07936542359de365c4b074fe29491aea62c2f
srivani <katukamsrivani@gmail.com>
2025-02-27 20:51:56 +0530

    Added BEX Token API

---
+++ src/main/java/com/example/transactionapi/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.example.transactionapi.repository;
+
+import com.example.transactionapi.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
